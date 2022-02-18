468c26c85bb9bac056e120f7d3bfedad0ad2b5be
valentinooodev <valentinooo.dev@gmail.com>
2022-02-18 14:18:33 +0700

    fix health

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,5 @@ app_name = 'api'
 urlpatterns = [
     path('login/', LoginAPIView.as_view(), name='login'),
     path('auth/verify/', AuthenticationAPIView.as_view(), name='auth'),
-    path('health', CheckHealth.as_view(), name='check_health'),
+    path('health/', CheckHealth.as_view(), name='check_health'),
 ]
